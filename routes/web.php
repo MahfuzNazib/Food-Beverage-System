62bcc72b04039b8c4d2b2f155abf4dde04963089
Nazib Mahfuz <nazibmahfuz60@gmail.com>
2021-08-01 12:44:41 +0600

    Init router change

--- routes/web.php
+++ routes/web.php
@@ -18,31 +18,16 @@ Route::post('/do-logout', [LogoutController::class, 'do_logout'])->name('do.logo
 Route::group(['prefix' => 'Dashboard', 'middleware' => 'auth'], function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
-	//user management route start
+	//user management route 
     require_once 'backend/user_management.php';
-    //user management route end
 
-	// Role Management Route Start
+	// Role Management Route 
 	require_once 'backend/role_management.php';
-	// Role Management Route End
 
-	// Meal Management Route Start
+	// Meal Management Route 
 	require_once 'backend/meal_management.php';
-	// Meal Management Route End
 });
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-// Auth::routes();
 
 Route::get('/home', 'App\Http\Controllers\HomeController@index')->name('home');
-
-// Route::group(['middleware' => 'auth'], function () {
-// 	Route::resource('user', 'App\Http\Controllers\UserController', ['except' => ['show']]);
-// 	Route::get('profile', ['as' => 'backend.modules.profile.edit', 'uses' => 'App\Http\Controllers\ProfileController@edit']);
-// 	Route::put('profile', ['as' => 'backend.modules.profile.update', 'uses' => 'App\Http\Controllers\ProfileController@update']);
-// 	Route::get('upgrade', function () {return view('pages.upgrade');})->name('upgrade'); 
-// 	 Route::get('map', function () {return view('backend.modules.maps');})->name('map');
-// 	 Route::get('icons', function () {return view('backend.modules.icons');})->name('icons'); 
-// 	 Route::get('table-list', function () {return view('backend.modules.tables');})->name('table');
-// 	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'App\Http\Controllers\ProfileController@password']);
-// });
